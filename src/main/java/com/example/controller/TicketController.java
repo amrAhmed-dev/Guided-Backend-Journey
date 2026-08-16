9e28a077cc71500edebfd432fa121dc171c447d8
amrAhmed-dev <amrahmed8a@gmail.com>
2026-08-16 22:41:28 +0300

    updating and changing the  controller class name

--- src/main/java/com/example/controller/TicketController.java
+++ src/main/java/com/example/controller/TicketController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Ticketcontorller {
+public class TicketController {
     @Autowired
     private TicketService ticketService;
 
