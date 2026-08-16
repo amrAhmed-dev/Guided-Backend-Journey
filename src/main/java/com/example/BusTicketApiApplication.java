f3aeb486ce54cedac3cf36b12a5f2e963140a50e
amrAhmed-dev <amrahmed8a@gmail.com>
2026-08-16 22:26:29 +0300

    update the name of main , update the pom.xml , clean up the project

--- src/main/java/com/example/BusTicketApiApplication.java
+++ src/main/java/com/example/BusTicketApiApplication.java
@@ -1,13 +1,13 @@
-package com.example.BustTicketApi;
+package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BustTicketApiApplication {
+public class BusTicketApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BustTicketApiApplication.class, args);
+		SpringApplication.run(BusTicketApiApplication.class, args);
 	}
 
 }
